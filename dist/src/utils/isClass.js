2a57f67f485229097ffa44257837a6c9bbc8f33d
abdrah124 <abdmanharits@gmail.com>
2024-06-23 22:14:34 +0700

    Delete dist folder and vercel.json

--- dist/src/utils/isClass.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isClass = void 0;
-const isClass = (cls) => {
-    var _a;
-    return (_a = cls === null || cls === void 0 ? void 0 : cls.toString()) === null || _a === void 0 ? void 0 : _a.includes("class");
-};
-exports.isClass = isClass;
-//# sourceMappingURL=isClass.js.map
